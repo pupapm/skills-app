d6b2ee5f113274f8cf3453167e6798014e53d18b
Pupa Mechuchep <mechuchepphupa@gmail.com>
2026-05-05 11:34:48 +0700

    add production cors origin

--- backend/main.go
+++ backend/main.go
@@ -21,7 +21,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173",
+		AllowOrigins: "http://localhost:5173, https://skills-app-nine.vercel.app",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 	}))
